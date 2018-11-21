64b9bb9dfc6881dbea09d0f28d2237ec421f1d13
*Andriy <*skyba0andriy@gmail.com*>
2018-11-21 14:22:31 +0200

    Optimize code

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,29 @@
-$(document).ready(function() {
+$(document).ready(() => {
 	a();
 })
 
 function a() {
-	$("#search").on("click", function(){
+	$("#search").on("click", () => {
 		let searchTerm = $("#searchTerm").val();
-		let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+		let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchTerm}&format=json&callback=?`;
 		$.ajax({
 			type: "GET",
 			url: url,
 			async: false,
 			dataType: "json",
-			success : function(data) {
+			success : data => {
 					$("#output").empty();
-					if(searchTerm !== "" && data[1].length !== 0) {
+					if(data[1].length !== 0) {
 						for(let i = 0; i < data[1].length; i++){
-							$("#output").prepend("<div><a href = "+ 
-															data[3][i] +"><h2 style = 'color: orange;'>"+ 
-															data[1][i] +"</h2></a><p>"+ 
-															data[2][i] +"</p></div><hr>");
+							$("#output").prepend(`<div><a href = ${data[3][i]}><h2 style = 'color: orange;'>
+																 ${data[1][i]}</h2></a><p> 
+																 ${data[2][i]}</p></div><hr>`);
 						}
 					}else {
 						$("#output").prepend("<h1 style = 'color: red; text-align: center;'>Error 404 &#9785<h1>");
 					}	
 			},
-			error : function(errorMessage) {
+			error : errorMessage => {
 				$("#output").html(errorMessage);
 			}
 		})
