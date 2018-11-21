d7996e9ce7ad2670e65161b2d338acfdd0f385f6
*Andriy <*skyba0andriy@gmail.com*>
2018-11-21 22:11:36 +0200

    delete style in js file and add in sass file

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
-	a();
+	wikiApi();
 })
 
-function a() {
+function wikiApi() {
 	$("#search").on("click", () => {
 		let searchTerm = $("#searchTerm").val();
 		let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchTerm}&format=json&callback=?`;
@@ -13,14 +13,14 @@ function a() {
 			dataType: "json",
 			success(data) {
 					$("#output").empty();
-					if(data[1].length !== 0) {
+					if(searchTerm !== "" && data[1].length !== 0) {
 						for(let i = 0; i < data[1].length; i++){
-							$("#output").prepend(`<div><a href = ${data[3][i]}><h2 style = 'color: orange;'>
+							$("#output").prepend(`<div><a href = ${data[3][i]}><h2 class='link'>
 																 ${data[1][i]}</h2></a><p> 
 																 ${data[2][i]}</p></div><hr>`);
 						}
 					}else {
-						$("#output").prepend("<h1 style = 'color: red; text-align: center;'>Error 404 &#9785<h1>");
+						$("#output").prepend("<h1 class='errorText'>Error 404 &#9785<h1>");
 					}	
 			},
 			error(errorMessage) {
